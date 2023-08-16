f41178388c66f31d9b7f811683aee99544f7d923
박준현 <4rne5@duck.com>
2023-08-16 09:10:20 +0900

    Initial Commit

---
+++ BIND-Diary/BIND_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIND_DiaryApp.swift
+//  BIND-Diary
+//
+//  Created by 4rNe5 on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct BIND_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
